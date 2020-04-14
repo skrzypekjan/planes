10d120c29133a97cfa5dc6a46862933417c7da45
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-04-14 23:18:21 +0200

    new component for displaying the form

---
+++ src/app/flights/new-flight/new-flight.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-new-flight',
+  templateUrl: './new-flight.component.html',
+  styleUrls: ['./new-flight.component.scss']
+})
+export class NewFlightComponent {
+
+  constructor(private dialogRef: MatDialogRef<NewFlightComponent>) { }
+
+
+}
