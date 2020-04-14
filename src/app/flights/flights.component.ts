1b1997bd20b4cd6a085dbf592c7f3d8545e77770
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-04-14 23:21:16 +0200

    component modification after adding new ones  

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {FlightsService} from '../core/services/flights.service';
 import {Observable} from 'rxjs';
 import {Flight} from '../models/flight.model';
+import {MatDialog} from '@angular/material/dialog';
+import {NewFlightComponent} from './new-flight/new-flight.component';
 
 @Component({
   selector: 'app-flights',
@@ -11,6 +13,11 @@ import {Flight} from '../models/flight.model';
 export class FlightsComponent {
   flights$: Observable<Flight[]> = this.flightsService.getFlights();
 
-  constructor(private flightsService: FlightsService) { }
+  constructor(
+    private dialog: MatDialog,
+    private flightsService: FlightsService) { }
 
+  openNewFlightModel(){
+    this.dialog.open(NewFlightComponent);
+  }
 }
--- src/app/flights/flights.module.ts
+++ src/app/flights/flights.module.ts
@@ -4,16 +4,24 @@ import { FlightsComponent } from './flights.component';
 import {RouterModule} from '@angular/router';
 import {MaterialModule} from '../material/material.module';
 import { FlightCardComponent } from './flight-card/flight-card.component';
+import { NewFlightComponent } from './new-flight/new-flight.component';
+import { FlightsFormComponent } from './flights-form/flights-form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [FlightsComponent, FlightCardComponent],
-  exports: [FlightsComponent],
+  declarations: [FlightsComponent, FlightCardComponent, FlightsFormComponent,
+    NewFlightComponent, FlightsFormComponent],
+  exports: [FlightsComponent, FlightsFormComponent
+  ],
+  entryComponents: [NewFlightComponent],
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class FlightsModule { }
