47fb14f8f0c14fc48a77bba367bf339cd823ac86
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-04-23 16:10:52 +0200

    adding a flight creation method to firebase

--- src/app/flights/new-flight/new-flight.component.ts
+++ src/app/flights/new-flight/new-flight.component.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {FlightsFormComponent} from '../flights-form/flights-form.component';
+import {FlightsService} from '../../core/services/flights.service';
 
 @Component({
   selector: 'app-new-flight',
@@ -11,7 +12,24 @@ import {FlightsFormComponent} from '../flights-form/flights-form.component';
 export class NewFlightComponent {
 
   @ViewChild('flightForm') flightForm: FlightsFormComponent;
-  constructor(private dialogRef: MatDialogRef<NewFlightComponent>) { }
 
+  constructor(
+    private dialogRef: MatDialogRef<NewFlightComponent>,
+    private flightsService: FlightsService
+  ) { }
+
+  createFlight(){
+    console.log(this.flightForm);
+    this.flightsService.addFlight(this.flightForm.form.value)
+      .then(this.onCreatingSucces.bind(this), this.onCreatingFailure.bind(this));
+  }
+
+  private onCreatingSucces(){
+    this.dialogRef.close();
+  }
+
+  private onCreatingFailure(){
+    console.log('some error');
+  }
 
 }
