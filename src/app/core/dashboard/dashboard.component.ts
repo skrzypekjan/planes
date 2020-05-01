926ee5c21c775b60ecf05564e9960744ff62fe27
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-05-01 15:29:15 +0200

    display of the logged in user

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  user = this.authService.user;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toast: MatSnackBar
+  ) {}
 
-  ngOnInit(): void {
+  logout() {
+    this.authService.logout()
+      .then(() => this.router.navigate(['/login']))
+      .then(user => this.toast.open('You have been logged out correctly! :)', '', {panelClass: 'toast-success'}))
+      .catch(error => this.toast.open(error.message, '', {panelClass: 'toast-error'}));
   }
-
 }
