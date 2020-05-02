3bb521d2f970ded4f2c5afd833a2fc9591bb6bc5
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-05-02 17:11:01 +0200

    adding wild card solution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {DashboardComponent} from './core/dashboard/dashboard.component';
 import {FlightsComponent} from './flights/flights.component';
 import {EditFlightComponent} from './flights/edit-flight/edit-flight.component';
 import {AuthGuard} from './core/services/auth.guard';
+import {PageNotFoundComponent} from './core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
@@ -17,7 +18,8 @@ const routes: Routes = [
       {path: '', redirectTo: 'flights', pathMatch: 'full'},
       {path: 'flights', loadChildren: './flights/flights.module#FlightsModule'},
     ]
-  }
+  },
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
